9cf4523bc9fce77c58b7de387a46d66a9bcb42ed
unknown <jvsantospessoal@gmail.com>
2022-11-23 16:26:03 -0300

    Adicionando a introducao a variaveis, ensinando como podemos utilizar variaveis numericas em R.

---
+++ Introdução ao R/02_Variaveis.R
@@ -0,0 +1,30 @@
+# VARIAVEIS
+
+# Variaveis sao utilizadas para guardar valores que utilizaremos no nosso codigo futuramente
+
+# definimos duas variaveis: a e b 
+a = 5
+b = 3
+
+# somamos as duas variaveis 
+a + b
+
+# =========================================================================================================
+# TIPOS DE VARIAVEIS
+
+# 1. NUMERIC: Numeros no geral, com virgula ou sem (inteiros ou reais)
+
+x <- 1
+
+class(x) # Esse comando dira qual eh o tipo de "x"
+cat(x) # Esse comando colocara o valor de "x" na tela
+
+is.numeric(x) # Dira se o conteudo colocado eh NUMERIC ou nao, se for ele dira TRUE
+
+y <- 1E2 # 10 elevado a 2 = 100
+
+cat(y)
+class(y)
+
+is.numeric(y)
+
