e1407cc83df6306454c665e183c5b37855206b4e
unknown <jvsantospessoal@gmail.com>
2022-11-23 16:49:27 -0300

    Terminando a primeira parte, introduzindo novos tipos de variaveis.

--- Introdução ao R/02_Variaveis.R
+++ Introdução ao R/02_Variaveis.R
@@ -9,6 +9,17 @@ b = 3
 # somamos as duas variaveis 
 a + b
 
+# OBS: O nome de variaveis geralmente segue uma norma (criada por programadores) que geralmente segue o seguinte padrao:
+# nomeDeVariavel, variavel1, variavel2, primeiroNome, variavelNova, listaDeAlunos
+
+# isso eh chamado de camelCase, onde a separacao das palavras eh feita utilizando uma letra maiuscula no inicio, alem disso
+# NAO utilizamos acentuacao e nem pontuacao para variaveis. Geralmente tente evitar utilizar acentuacao nos comentarios.
+
+# tambem existe o snake case:
+
+# primeira_nota, segunda_nota, variavel_criada, teste_de_variavel
+
+
 # =========================================================================================================
 # TIPOS DE VARIAVEIS
 
@@ -27,4 +38,28 @@ cat(y)
 class(y)
 
 is.numeric(y)
+
+# 2. CHARACTER / STRING: Utilizamos esse tipo para trabalhar com texto, para indicar que estamos utilizando um caractere
+# devemos utilizar aspas
+
+nome <- "Joao"
+
+class(nome) # diz qual eh o tipo do objeto 
+cat(nome)   # coloca o objeto na tela
+
+# 3. LOGICAL: Tipo logico, teremos duas possibilidades: V e F, verdadeiro e Falso.
+
+variavelVerdadeira <- TRUE
+
+class(variavelVerdadeira)
+cat(variavelVerdadeira)
+
+is.logical(variavelVerdadeira)
+
+variavelFalsa <- FALSE
+
+class(variavelFalsa)
+cat(variavelFalsa)
+
+is.logical(variavelFalsa)
 
